e465f396ca4cab67d94dd666bcec2b4899cf3fe6
fadel <fafadel230@gmail.com>
2025-10-09 12:16:42 -1200

    day-2

---
+++ day-2/script.js
@@ -0,0 +1,23 @@
+var name = "Mouhamed Fadel BA";
+let age = 23;
+const isStudent = true;
+let favoriteProgrammingLanguage = "java and javascript";
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(favoriteProgrammingLanguage);
+
+// age = 22;
+// isStudent = false;
+
+let person = {
+    name: "Mouhamed Fadel BA",
+    age: 23,
+    isStudent: true,
+    favoriteProgrammingLanguage: "java and javascript"
+};
+
+let dataBase = [person.name,age,isStudent,"like",favoriteProgrammingLanguage,'✨'];
+
+console.log(dataBase);
